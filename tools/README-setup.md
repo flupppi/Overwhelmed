9192da4627d8fda901fbbc4d1fc7a29880ae0c9d
flupppi <kalchscf@hochschule-trier.de>
2025-03-27 13:31:16 +0100

    Added automatic setup script

---
+++ tools/README-setup.md
@@ -0,0 +1,6 @@
+### 🛠 Setting up Remotes
+
+If you're cloning this repo on a new machine or sharing it, run the setup script:
+
+```bash
+./tools/setup-remotes.sh
---
+++ tools/setup-remotes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add codeberg-pages git@codeberg.org:Overwhelmed/pages.git
+git remote add codeberg-mobile git@codeberg.org:Overwhelmed/overwhelmed-mobile.git
+git remote add codeberg-desktop git@codeberg.org:Overwhelmed/overwhelmed-desktop.git
+git remote add codeberg-drones git@codeberg.org:Overwhelmed/overwhelmed-drones.git
+
